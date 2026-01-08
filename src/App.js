7827ccee425ce082b68bff4485a32e8b6d16d0f7
Nisharani26 <nisharani262004@gmail.com>
2026-01-09 00:26:07 +0530

    created News

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import Newsitems from './components/Newsitems'
+import News from './components/News'
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Newsitems from './Newsitems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        <Newsitems/>
+      </div>
+    )
+  }
+}
+
+export default News
