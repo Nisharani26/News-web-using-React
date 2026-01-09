4f7eb5977879e4c8ead184821bdcdfe8d50bd4c2
Nisharani26 <nisharani262004@gmail.com>
2026-01-09 10:16:33 +0530

    cards are created

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import Newsitems from './Newsitems'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        <Newsitems/>  
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <div className='container  my-3'>
+                    <h2>Headlines</h2>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <Newsitems title="myTitle" description="myDescription" />
+                        </div>
+                        <div className='col-md-4'>
+                            <Newsitems title="myTitle" description="myDescription" />
+                        </div>
+                        <div className='col-md-4'>
+                            <Newsitems title="myTitle" description="myDescription" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
