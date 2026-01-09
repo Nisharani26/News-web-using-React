af632e97a3633cc94735b37b43298e9afc928629
Nisharani26 <nisharani262004@gmail.com>
2026-01-09 12:20:29 +0530

    news api are loaded

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
     render() {
-    let {title , description} = this.props;
+    let {title , description ,imgUrl,source_url} = this.props;
         return (
             <div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src={imgUrl} className="card-img-top" alt="the image is not loading" />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href={source_url} className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
